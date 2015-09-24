1d1e07b9c8a632999e3ca333f7cdbf9387ccdcec
Christoph Lehmann <christoph.lehmann@ufz.de>
2015-09-24 17:46:09 +0200

    [Extrapolation] fixed private to public

--- NumLib/Extrapolation/LocalNodalDOF.h
+++ NumLib/Extrapolation/LocalNodalDOF.h
@@ -6,6 +6,7 @@ namespace NumLib {
 
 class LocalNodalDOF
 {
+public:
     virtual std::vector<double> getElementNodalValues() = 0;
     virtual std::vector<double> getElementNodalValues(unsigned component) = 0;
 };
