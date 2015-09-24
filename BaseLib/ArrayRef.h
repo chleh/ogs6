802acec340436eec5e0891505cf39f21c9e6aa9d
Christoph Lehmann <christoph.lehmann@ufz.de>
2015-09-24 12:36:58 +0200

    [Base] added (incomplete) wrapper for arrays with runtime specified size

--- BaseLib/ArrayRef.h
+++ BaseLib/ArrayRef.h
@@ -6,12 +6,91 @@
 namespace BaseLib
 {
 
-/*
- * TODO: provide implementation for dynamic sizes
+template<typename T, std::size_t... Sizes>
+class ArrayRef;
+
+
+/**
+ * Dynamically sized non-owning C-array wrapper
  */
+template<typename T>
+class ArrayRef<T>
+{
+public:
+    // Type
+    using value_type = T;
+    using size_type  = std::size_t;
+    using difference_type = std::ptrdiff_t;
+    using reference = value_type&;
+    using const_reference = const value_type&;
+    using pointer = value_type*;
+    using const_pointer = const value_type*;
+    using iterator = value_type*;
+    using const_iterator = const value_type*;
+    using reverse_iterator = std::reverse_iterator<iterator>;
+    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+
+    explicit ArrayRef(T* data) : _data{data} {}
+
+    ArrayRef(ArrayRef<T> const&) = default; // all copies of an instance will point to the __same__ data!
+    ArrayRef(ArrayRef<T>&&) = default;      // move will actually copy this reference
+
+    ArrayRef<T>& operator=(ArrayRef<T> const&) = default; // all copies of an instance will point to the __same__ data!
+    ArrayRef<T>& operator=(ArrayRef<T> &&) = default;     // move will actually copy this reference
+
+    iterator        begin()       { return _data; }
+    const_iterator  begin() const { return _data; }
+    const_iterator cbegin() const { return _data; }
+    iterator          end()       { return _data + _size; }
+    const_iterator    end() const { return _data + _size; }
+    const_iterator   cend() const { return _data + _size; }
+
+    reverse_iterator        rbegin()       { return _data + _size - 1; }
+    const_reverse_iterator  rbegin() const { return _data + _size - 1; }
+    const_reverse_iterator crbegin() const { return _data + _size - 1; }
+    reverse_iterator          rend()       { return _data - 1; }
+    const_reverse_iterator    rend() const { return _data - 1; }
+    const_reverse_iterator   crend() const { return _data - 1; }
+
+    const_pointer data() const { return _data; }
+    pointer       data()       { return _data; }
+
+    reference                 front()       { return *_data; }
+    constexpr const_reference front() const { return *_data; }
+    reference                  back()       { return _data[_size-1]; }
+    constexpr const_reference  back() const { return _data[_size-1]; }
+
+    reference       operator[](std::size_t pos)       { return _data[pos]; }
+    const_reference operator[](std::size_t pos) const { return _data[pos]; }
+
+    reference at(std::size_t pos) {
+        if (pos >= _size) throw std::out_of_range("Given index exceeds array size.");
+        return _data[pos];
+    }
+    const_reference at (std::size_t pos) const {
+        if (pos >= _size) throw std::out_of_range("Given index exceeds array size.");
+        return _data[pos];
+    }
+
+    constexpr bool empty() const { return _size == 0; }
+    constexpr size_type size() const { return _size; }
+    constexpr size_type max_size() const { return _size; }
+
+    void fill(const T& value)     { for (auto& v : *this) v = value; }
+    void swap(ArrayRef<T>& other) {
+        std::swap(_size, other._size);
+        std::swap(_data, other._data);
+    }
+
+private:
+    size_type _size;
+    T* _data;
+};
+
+
 
 template<typename T, std::size_t Size>
-class ArrayRef
+class ArrayRef<T, Size>
 {
 public:
     // Type
@@ -30,10 +109,10 @@ public:
     explicit ArrayRef(T* data) : _data{data} {}
 
     ArrayRef(ArrayRef<T, Size> const&) = default; // all copies of an instance will point to the __same__ data!
-    ArrayRef(ArrayRef<T, Size>&&) = delete;       // no moves
+    ArrayRef(ArrayRef<T, Size>&&) = default;      // move will actually copy this reference
 
     ArrayRef<T, Size>& operator=(ArrayRef<T, Size> const&) = default; // all copies of an instance will point to the __same__ data!
-    ArrayRef<T, Size>& operator=(ArrayRef<T, Size> &&) = delete;      // no move assignment
+    ArrayRef<T, Size>& operator=(ArrayRef<T, Size> &&) = default;     // move will actually copy this reference
 
     iterator        begin()       { return _data; }
     const_iterator  begin() const { return _data; }
@@ -73,7 +152,7 @@ public:
     constexpr size_type size() const { return Size; }
     constexpr size_type max_size() const { return Size; }
 
-    void fill(const T& value)               { for (auto& v : *this) v = value; }
+    void fill(const T& value)           { for (auto& v : *this) v = value; }
     void swap(ArrayRef<T, Size>& other) { std::swap(_data, other._data); }
 
 private:
