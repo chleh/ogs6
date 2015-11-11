74978cff5937702ecf299e706c2690d35a63bed9
Christoph Lehmann <christoph.lehmann@ufz.de>
2015-11-11 12:56:48 +0100

    [pardiso] faster apply known solution

--- MathLib/LinAlg/Pardiso/PardisoTools.cpp
+++ MathLib/LinAlg/Pardiso/PardisoTools.cpp
@@ -10,6 +10,7 @@
 #include "PardisoTools.h"
 #include "MathLib/LinAlg/Sparse/LOLMatrix.h"
 
+#include "logog/include/logog.hpp"
 
 namespace MathLib
 {
@@ -21,37 +22,31 @@ void applyKnownSolution(LOLMatrix &A, DenseVector<double> &b,
 {
     using IT = LOLMatrix::IndexType;
 
-    auto const n_rows = A.getNRows();
+    //A(k, j) = 0.
+    // set row to zero
+    // A.setRowZero(row_id);
+    for (auto row_id : vec_knownX_id)
+        for (auto& e : A.getRow(row_id)) e.value = 0.0;
+
+    auto AT = A.transpose();
 
     for (std::size_t ix=0; ix<vec_knownX_id.size(); ix++)
     {
         IT const row_id = vec_knownX_id[ix];
         auto const x = vec_knownX_x[ix];
 
-        //A(k, j) = 0.
-        for (auto& e : A.getRow(row_id)) e.value = 0.0;
-        // A.setRowZero(row_id);
-
-        //b_i -= A(i,k)*val, i!=k
-        for (std::size_t i=0; i<n_rows; i++)
-        {
-            auto& row = A.getRow(i);
-
-            for (auto& e : row) {
-                if (e.col_idx == row_id) {
-                    b[i] -= e.value * x;
-                    e.value = 0.0;
-                    break;
-                }
-            }
+        // b_i -= A(i,k)*val, i!=k
+        // set column to zero, subtract from rhs
+        for (auto& e : AT.getRow(row_id)) {
+            b[e.col_idx] -= e.value * x;
+            e.value = 0.0;
         }
 
-        //A(k, k) = 1.0
-        A.setValue(row_id, row_id, 1.0);
-
-        //b_k = val
         b[row_id] = x;
+        AT.setValue(row_id, row_id, 1.0);
     }
+
+    A = AT.transpose();
 }
 
 }
