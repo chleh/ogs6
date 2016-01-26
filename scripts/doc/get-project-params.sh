3424a30138e308844b1c85e1a6f585dc526b1a68
Christoph Lehmann <christoph.lehmann@ufz.de>
2016-01-26 09:47:24 +0100

    [scri/docr] extended script slightly

--- scripts/doc/get-project-params.sh
+++ scripts/doc/get-project-params.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-cat <<EOF \
+#color="--color=always"
+color=""
+
+cat <<"EOF" \
 | grep -r . \
     --include '*.h' \
     --include '*.cpp' \
     --exclude-dir '.git' \
     --exclude-dir 'Tests' \
     --exclude 'ConfigTreeNew*.*' \
-    -f - -r -w -o
+    -f - -r -n -o $color \
+| sed -e 's_::_@@_g' -e's_:\s\+_:_' | column -t -s: | sed -e 's_@@_::_g'
+^\s*//! \\ogs_project_file_parameter{[a-z_0-9]\+}$
+checkConfParam.*)
+getConfAttribute.*)
 getConfParam.*)
 getConfSubtree.*)
-peekConfParam.*)
-checkConfParam.*)
 ignoreConfParam.*)
+peekConfParam.*)
 EOF
 
