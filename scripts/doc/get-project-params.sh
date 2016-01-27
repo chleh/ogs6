e6720108b32ee0afe840d16f7a6ba1171c903b08
Christoph Lehmann <christoph.lehmann@ufz.de>
2016-01-27 16:32:42 +0100

    [scr] added cmdline parameters

--- scripts/doc/get-project-params.sh
+++ scripts/doc/get-project-params.sh
@@ -1,22 +1,36 @@
 #!/bin/bash
 
+if [ $# -ne 2 ]; then
+    echo "USAGE: ${0##*/} SRCDIR DOCAUXDIR" >&2
+    exit 1
+fi
+
+further_script="`dirname "$0"`/check-project-params.py"
+srcdir="`readlink -f "$1"`"
+docauxdir="`readlink -f "$2"`"
+
 #color="--color=always"
 color=""
 
 cat <<"EOF" \
-| grep -r . \
+| grep -r $srcdir \
     --include '*.h' \
     --include '*.cpp' \
     --exclude-dir '.git' \
     --exclude-dir 'Tests' \
     --exclude 'ConfigTree*.*' \
     -f - -r -n -o $color \
-| sed -e 's_::_@@_g' -e's_:\s\+_:_' | column -t -s: | sed -e 's_@@_::_g'
+| cut -c $((`expr length "$srcdir"` + 2))- \
+| "$further_script" "$docauxdir"
 ^\s*//! \\ogs_project_file_parameter{[A-Za-z_0-9]\+}\( \\todo project_file_docu\)\?$
 checkConfParam.*)
 getConfAttribute.*)
 getConfParam.*)
 getConfSubtree.*)
+ignoreConfAttribute.*)
 ignoreConfParam.*)
 peekConfParam.*)
 EOF
+
+# format as table:
+# | sed -e 's_::_@@_g' -e's_:\s\+_:_' | column -t -s: | sed -e 's_@@_::_g'
