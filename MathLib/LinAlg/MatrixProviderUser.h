98771846ebb41313665ab68dee3cd25108605a7b
Christoph Lehmann <christoph.lehmann@ufz.de>
2016-02-28 12:05:21 +0100

    [MaL] added basic matrix spec handling interfaces

---
+++ MathLib/LinAlg/MatrixProviderUser.h
@@ -0,0 +1,45 @@
+// TODO
+#pragma once
+
+#include <cstddef>
+
+namespace MathLib
+{
+
+struct MatrixSpecifications
+{
+    std::size_t const nrows;
+    std::size_t const ncols;
+};
+
+class MatrixSpecificationsProvider
+{
+public:
+    virtual MatrixSpecifications getMatrixSpecifications() const = 0;
+
+    virtual ~MatrixSpecificationsProvider() = default;
+};
+
+template<typename Matrix, typename Vector>
+class MatrixProvider
+{
+public:
+    virtual Matrix& getMatrix(std::size_t const size, std::size_t& id) = 0;
+    virtual void releaseMatrix(std::size_t const id) = 0;
+
+    virtual void setMatrixInfoProvider(MatrixSpecificationsProvider const& info_prvd) = 0;
+
+    virtual ~MatrixProvider() = default;
+};
+
+template<typename Matrix, typename Vector>
+class MatrixUser
+{
+public:
+    virtual void setMatrixProvider(MatrixProvider<Matrix, Vector>& prvd) = 0;
+
+    virtual ~MatrixUser() = default;
+};
+
+
+} // namespace MathLib
