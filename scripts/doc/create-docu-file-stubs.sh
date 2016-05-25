8e6c4aead7d04543a0735f741eacae67696a3ee4
Christoph Lehmann <christoph.lehmann@ufz.de>
2016-05-25 15:12:31 +0200

    [scr/doc] some changes

--- scripts/doc/create-docu-file-stubs.sh
+++ scripts/doc/create-docu-file-stubs.sh
@@ -6,14 +6,21 @@ base="Documentation/ProjectFile"
 
 while read -r fn lno content; do
     [ "$content" = "${content#//!}" ] && continue
-    tag_name="$(echo "$content" | sed -n -e 'sX^//! \\ogs_project_file_parameter{\([a-z_0-9]\+\)}$X\1Xp')"
+    tag_name="$(echo "$content" | sed -n -e 'sX^//! \\ogs_project_file_parameter{\([A-Za-z_0-9]\+\)}$X\1Xp')"
     [ -z "$tag_name" ] && continue
     tag_name="${tag_name//__/\/}"
     echo "$base/$tag_name"
 done \
 | sort -r \
 | while read path; do
-    mkdir -p "`dirname "$path"`"
+    dn="`dirname "$path"`"
+    if [ ! -d "$dn" ]; then
+        mkdir -p "$dn"
+
+        bn="`basename "$path"`"
+        echo "creating $path/_$bn.md"
+        echo '\todo document' >"$path/_$bn.md"
+    fi
 
     if [ -d "$path" ]; then
         bn="`basename "$path"`"
@@ -24,5 +31,9 @@ done \
     elif ! [ -f "$path.md" ]; then
         echo "creating $path.md"
         echo '\todo document' >"$path.md"
+    fi
+
+    if [ -d "$path" ] && [ -f "$path.md" ]; then
+        echo "ERROR: both $path and $path.md exist!" >&2
     fi
 done
--- scripts/doc/get-project-params.sh
+++ scripts/doc/get-project-params.sh
@@ -12,7 +12,7 @@ cat <<"EOF" \
     --exclude 'ConfigTree*.*' \
     -f - -r -n -o $color \
 | sed -e 's_::_@@_g' -e's_:\s\+_:_' | column -t -s: | sed -e 's_@@_::_g'
-^\s*//! \\ogs_project_file_parameter{[a-z_0-9]\+}\( \\todo project_file_docu\)\?$
+^\s*//! \\ogs_project_file_parameter{[A-Za-z_0-9]\+}\( \\todo project_file_docu\)\?$
 checkConfParam.*)
 getConfAttribute.*)
 getConfParam.*)
