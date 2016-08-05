ea46f6840136134cffd5052c884b668cc9b1882a
Christoph Lehmann <christoph.lehmann@ufz.de>
2016-08-05 08:13:34 +0200

    [PL] introduced Eigen::Map utils

--- ProcessLib/CentralDifferencesJacobianAssembler.cpp
+++ ProcessLib/CentralDifferencesJacobianAssembler.cpp
@@ -14,26 +14,49 @@
 
 Eigen::Map<
     Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>
-toMatrix(std::vector<double>& data, std::size_t num_r_c)
+toZeroedMatrix(std::vector<double>& data, Eigen::Index rows, Eigen::Index cols)
 {
     assert(data.empty());  // in order that resize fills the vector with zeros.
-    data.resize(num_r_c * num_r_c);
-    return {data.data(), num_r_c, num_r_c};
+    data.resize(rows * cols);
+    return {data.data(), rows, cols};
+}
+
+Eigen::Map<const Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic,
+                               Eigen::RowMajor>>
+toMatrix(std::vector<double> const& data, Eigen::Index rows, Eigen::Index cols)
+{
+    assert(static_cast<Eigen::Index>(data.size()) == rows * cols);
+    return {data.data(), rows, cols};
+}
+
+Eigen::Map<
+    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>
+toMatrix(std::vector<double>& data, Eigen::Index rows, Eigen::Index cols)
+{
+    assert(static_cast<Eigen::Index>(data.size()) == rows * cols);
+    return {data.data(), rows, cols};
+}
+
+Eigen::Map<Eigen::VectorXd> toZeroedVector(std::vector<double>& data,
+                                           Eigen::Index rows)
+{
+    assert(data.empty());  // in order that resize fills the vector with zeros.
+    data.resize(rows);
+    return {data.data(), rows};
+}
+
+Eigen::Map<const Eigen::VectorXd> toVector(std::vector<double> const& data,
+                                           Eigen::Index rows)
+{
+    assert(static_cast<Eigen::Index>(data.size()) == rows);
+    return {data.data(), rows};
 }
 
 Eigen::Map<Eigen::VectorXd> toVector(std::vector<double>& data,
-                                     std::size_t num_r_c)
+                                     Eigen::Index rows)
 {
-    assert(data.empty());  // in order that resize fills the vector with zeros.
-    data.resize(num_r_c);
-    return {data.data(), num_r_c};
-}
-
-Eigen::Map<const Eigen::VectorXd> toConstVector(std::vector<double> const& data,
-                                                std::size_t num_r_c)
-{
-    assert(data.size() == num_r_c);
-    return {data.data(), num_r_c};
+    assert(static_cast<Eigen::Index>(data.size()) == rows);
+    return {data.data(), rows};
 }
 
 namespace ProcessLib
@@ -47,12 +70,12 @@ void CentralDifferencesJacobianAssembler::assembleWithJacobian(
     std::vector<double>& local_Jac_data)
 {
     auto const eps = 1e-8;
-    auto const num_r_c = local_x_data.size();
+    auto const num_r_c = static_cast<Eigen::Index>(local_x_data.size());
 
     std::vector<double> local_x_perturbed_data(local_x_data);
-    auto local_Jac = toMatrix(local_Jac_data, num_r_c);
-    auto local_x = toConstVector(local_x_data, num_r_c);
-    auto local_xdot = toConstVector(local_xdot_data, num_r_c);
+    auto local_Jac = toZeroedMatrix(local_Jac_data, num_r_c, num_r_c);
+    auto local_x = toVector(local_x_data, num_r_c);
+    auto local_xdot = toVector(local_xdot_data, num_r_c);
 
     for (std::size_t i = 0; i < num_r_c; ++i)
     {
@@ -66,13 +89,13 @@ void CentralDifferencesJacobianAssembler::assembleWithJacobian(
 
         local_x_perturbed_data[i] = local_x[i];
 
-        auto local_M_p = toMatrix(local_M_data, num_r_c);
-        auto local_K_p = toMatrix(local_K_data, num_r_c);
-        auto local_b_p = toVector(local_b_data, num_r_c);
+        auto local_M_p = toZeroedMatrix(local_M_data, num_r_c, num_r_c);
+        auto local_K_p = toZeroedMatrix(local_K_data, num_r_c, num_r_c);
+        auto local_b_p = toZeroedVector(local_b_data, num_r_c);
 
-        auto local_M_m = toMatrix(_local_M_data, num_r_c);
-        auto local_K_m = toMatrix(_local_K_data, num_r_c);
-        auto local_b_m = toVector(_local_b_data, num_r_c);
+        auto local_M_m = toZeroedMatrix(_local_M_data, num_r_c, num_r_c);
+        auto local_K_m = toZeroedMatrix(_local_K_data, num_r_c, num_r_c);
+        auto local_b_m = toZeroedVector(_local_b_data, num_r_c);
 
         // TODO check sizes!
         Eigen::MatrixXd const dM_dxi = (local_M_p - local_M_m) / (2.0 * eps);
@@ -94,9 +117,8 @@ void CentralDifferencesJacobianAssembler::assembleWithJacobian(
     local_assembler.assemble(t, local_x_data, local_M_data, local_K_data,
                              local_b_data);
 
-    // TODO no clear!
-    auto local_M = toMatrix(local_M_data, num_r_c);
-    auto local_K = toMatrix(local_K_data, num_r_c);
+    auto local_M = toMatrix(local_M_data, num_r_c, num_r_c);
+    auto local_K = toMatrix(local_K_data, num_r_c, num_r_c);
 
     local_Jac += local_M * dxdot_dx + local_K * dx_dx;
 }
