4f0646a81d18366ce111f6c20679e5acab5c08e0
Christoph Lehmann <christoph.lehmann@ufz.de>
2017-09-09 15:57:42 +0200

    extended test

--- python_bindings/test.py
+++ python_bindings/test.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+import numpy as np
+
 import ogs.solids as solids
 import ogs.ProcessLib as PL
 
@@ -19,3 +21,15 @@ lin_el_iso3 = MatModel(props)
 mat_state = lin_el_iso3.createMaterialStateVariables()
 
 help(lin_el_iso3)
+
+# constant params ==> actual time and position don't matter
+t = 0.0
+pos = PL.SpatialPosition()
+dt = 1.0
+eps_prev = np.array([ 0 ] * 6)
+eps = np.array([ 0 ] * 6)
+sigma_prev = np.array([ 0 ] * 6)
+
+res = lin_el_iso3.integrateStressPythonUniqueName(t, pos, dt, eps_prev, eps, sigma_prev, mat_state)
+
+print(res)
