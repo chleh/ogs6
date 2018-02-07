8b6312c69e7580db820f1bbb899ee07228819984
Christoph Lehmann <christoph.lehmann@ufz.de>
2018-02-07 16:36:38 +0100

    Finished empty pipe test

--- ProcessLib/IncompressibleStokesBrinkman/Tests.cmake
+++ ProcessLib/IncompressibleStokesBrinkman/Tests.cmake
@@ -1,1 +1,13 @@
 # Incompressible Stokes Brinkman
+AddTest(
+    NAME IncompressibleStokesBrinkman_pipe
+    PATH Elliptic/IncompressibleStokesBrinkman/pipe
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS pipe.prj
+    WRAPPER time
+    TESTER vtkdiff
+    REQUIREMENTS NOT OGS_USE_MPI
+    DIFF_DATA
+    pipe_ref.vtu pipe_pcs_0_ts_1_t_1.000000.vtu v_ref darcy_velocity 1e-12 1e-15
+    pipe_ref.vtu pipe_pcs_0_ts_1_t_1.000000.vtu p_ref pressure_interpolated 1e-9 1e-15
+)
--- Tests/Data/Elliptic/IncompressibleStokesBrinkman/pipe/prepare-meshes.py
+++ Tests/Data/Elliptic/IncompressibleStokesBrinkman/pipe/prepare-meshes.py
@@ -13,6 +13,9 @@ OGSPATH = "/home/lehmannc/prog/ogs6/github-chleh-PRs/build-release-eigenlis/bin"
 inlet_origin = (0.5, 1.0, 0.0)
 inlet_normal = (0.0, 1.0, 0.0)
 
+
+### create mesh
+
 subprocess.check_call([os.path.join(OGSPATH, "generateStructuredMesh"),
     "-e", "quad",
     "--lx", "1",
@@ -28,6 +31,8 @@ subprocess.check_call([os.path.join(OGSPATH, "createQuadraticMesh"),
     ])
 
 
+### create BCs
+
 reader = ps.XMLUnstructuredGridReader(FileName=["pipe.vtu"])
 
 enumerate_points = pre.EnumeratePoints(Input=reader)
@@ -37,10 +42,33 @@ slice1 = pre.PlaneSlice(inlet_origin, inlet_normal, Input=enumerate_points)
 
 to_unstructured = pre.AsUnstructuredGrid(Input=slice1)
 
-profile = pre.ParabolicProfileInX_1Component("velocity_y_inlet", Input=to_unstructured)
+def f_profile_v_y(coords):
+    return - (1.0 - coords[:,0]**2)
+
+profile = pre.NodalFunction("velocity_y_inlet", f_profile_v_y, Input=to_unstructured)
 profile.CopyArrays = 1 # copy input to output arrays
 
 # save data
 ps.SaveData("pipe_bc_inlet.vtu", proxy=profile, DataMode='Binary',
+    EncodeAppendedData=1,
+    CompressorType='ZLib')
+
+
+### create reference solution
+
+def f_profile_v(coords):
+    import numpy as np
+    profile = np.zeros((coords.shape[0], 2))
+    profile[:,1] = - (1.0 - coords[:,0]**2)
+    return profile
+
+def f_profile_p(coords):
+    return 4.0 * coords[:,1]
+
+profile_v = pre.NodalFunction("v_ref", f_profile_v, Input=reader)
+profile_p = pre.NodalFunction("p_ref", f_profile_p, Input=profile_v)
+profile_p.CopyArrays = 1
+
+ps.SaveData("pipe_ref.vtu", proxy=profile_p, DataMode='Binary',
     EncodeAppendedData=1,
     CompressorType='ZLib')
