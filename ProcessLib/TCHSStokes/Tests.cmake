032b784db425af5a3a1191c541a63877a1f9ad86
Christoph Lehmann <christoph.lehmann@ufz.de>
2018-02-28 15:42:29 +0100

    [PL] TCHSStokes pipe test

--- ProcessLib/TCHSStokes/Tests.cmake
+++ ProcessLib/TCHSStokes/Tests.cmake
@@ -1,12 +1,14 @@
 AddTest(
-    NAME TCHSStokes_test1
-    PATH Parabolic/TCHSStokes/test1
+    NAME TCHSStokes_pipe
+    PATH Parabolic/TCHSStokes/pipe
     EXECUTABLE ogs
     EXECUTABLE_ARGS pipe.prj
     WRAPPER time
     TESTER vtkdiff
     REQUIREMENTS NOT OGS_USE_MPI
     DIFF_DATA
-    pipe_ref.vtu pipe_pcs_0_ts_1_t_4.000000.vtu darcy_velocity darcy_velocity 1e-12 1e-15
-    pipe_ref.vtu pipe_pcs_0_ts_1_t_4.000000.vtu pressure_interpolated pressure_interpolated 1e-10 1e-15
+    pipe_ref.vtu pipe_pcs_0_ts_1_t_1.000000.vtu v_ref darcy_velocity 1e-11 1e-15
+    pipe_ref.vtu pipe_pcs_0_ts_1_t_1.000000.vtu p_ref pressure_interpolated 1e-15 1e-15
+    pipe_ref.vtu pipe_pcs_0_ts_1_t_1.000000.vtu T_ref temperature_interpolated 1e-15 1e-6
+    pipe_ref.vtu pipe_pcs_0_ts_1_t_1.000000.vtu x_ref mass_fraction_interpolated 1e-13 1e-13
 )
---
+++ Tests/Data/Parabolic/TCHSStokes/pipe/pipe.gml
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:42f477f2b7e692e7d459b9218fed0e06c9d348fbfd98d2d12d0457621e07ed27
+size 1003
