f612b38c47d2133c398d221816728fa6c0180040
Christoph Lehmann <christoph.lehmann@ufz.de>
2018-02-28 20:56:48 +0100

    [PL] added pipe test with porosity profile

--- ProcessLib/TCHSStokes/Tests.cmake
+++ ProcessLib/TCHSStokes/Tests.cmake
@@ -9,6 +9,21 @@ AddTest(
     DIFF_DATA
     pipe_ref.vtu pipe_pcs_0_ts_1_t_1.000000.vtu v_ref darcy_velocity 1e-11 1e-15
     pipe_ref.vtu pipe_pcs_0_ts_1_t_1.000000.vtu p_ref pressure_interpolated 1e-15 1e-15
-    pipe_ref.vtu pipe_pcs_0_ts_1_t_1.000000.vtu T_ref temperature_interpolated 1e-15 1e-6
+    pipe_ref.vtu pipe_pcs_0_ts_1_t_1.000000.vtu T_ref temperature_interpolated 1e-15 1e-14
     pipe_ref.vtu pipe_pcs_0_ts_1_t_1.000000.vtu x_ref mass_fraction_interpolated 1e-13 1e-13
+)
+
+AddTest(
+    NAME TCHSStokes_pipe_porosity_profile_eff_viscosity_ddp10_re1
+    PATH Parabolic/TCHSStokes/pipe-porosity-profile-eff-viscosity-ddp10-re1
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS pipe.prj
+    WRAPPER time
+    TESTER vtkdiff
+    REQUIREMENTS NOT OGS_USE_MPI
+    DIFF_DATA
+    pipe_ref.vtu pipe_pcs_0_ts_1_t_1.000000.vtu pressure_interpolated pressure_interpolated 1e-15 1e-14
+    pipe_ref.vtu pipe_pcs_0_ts_1_t_1.000000.vtu temperature_interpolated temperature_interpolated 1e-15 1e-13
+    pipe_ref.vtu pipe_pcs_0_ts_1_t_1.000000.vtu mass_fraction_interpolated mass_fraction_interpolated 1e-15 1e-13
+    pipe_ref.vtu pipe_pcs_0_ts_1_t_1.000000.vtu darcy_velocity darcy_velocity 1e-11 1e-15
 )
