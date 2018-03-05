1c78f6873d3e09a609677b7f423b5e47e2b249a5
Christoph Lehmann <christoph.lehmann@ufz.de>
2018-03-05 12:10:17 +0100

    added heat conduction test

--- ProcessLib/TCHSStokes/Tests.cmake
+++ ProcessLib/TCHSStokes/Tests.cmake
@@ -27,3 +27,23 @@ AddTest(
     pipe_ref.vtu pipe_pcs_0_ts_1_t_1.000000.vtu mass_fraction_interpolated mass_fraction_interpolated 1e-15 1e-13
     pipe_ref.vtu pipe_pcs_0_ts_1_t_1.000000.vtu darcy_velocity darcy_velocity 1e-11 1e-15
 )
+
+AddTest(
+    NAME TCHSStokes_pipe_zero_porosity
+    PATH Parabolic/TCHSStokes/pipe-zero-porosity
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS pipe.prj
+    WRAPPER time
+    TESTER vtkdiff
+    REQUIREMENTS NOT OGS_USE_MPI
+    DIFF_DATA
+    pipe_ref_500s.vtu pipe_pcs_0_ts_50_t_500.000000.vtu p_ref pressure_interpolated 1e-15 1e-15
+    pipe_ref_500s.vtu pipe_pcs_0_ts_50_t_500.000000.vtu T_ref temperature_interpolated 1e-15 4e-4
+    pipe_ref_500s.vtu pipe_pcs_0_ts_50_t_500.000000.vtu xmV_ref mass_fraction_interpolated 1e-15 1e-12
+    pipe_ref_500s.vtu pipe_pcs_0_ts_50_t_500.000000.vtu v_Darcy_ref darcy_velocity 1e-15 1e-15
+    #
+    pipe_ref_1000s.vtu pipe_pcs_0_ts_100_t_1000.000000.vtu p_ref pressure_interpolated 1e-15 1e-15
+    pipe_ref_1000s.vtu pipe_pcs_0_ts_100_t_1000.000000.vtu T_ref temperature_interpolated 1e-15 3e-4
+    pipe_ref_1000s.vtu pipe_pcs_0_ts_100_t_1000.000000.vtu xmV_ref mass_fraction_interpolated 1e-15 1e-12
+    pipe_ref_1000s.vtu pipe_pcs_0_ts_100_t_1000.000000.vtu v_Darcy_ref darcy_velocity 1e-15 1e-15
+)
