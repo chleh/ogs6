04786b0c6d6007827cb6b4fc090e9fcc8159ab01
Christoph Lehmann <christoph.lehmann@ufz.de>
2018-03-06 11:57:59 +0100

    EOS: added reference results, cleanup

--- ProcessLib/TCHSStokes/Tests.cmake
+++ ProcessLib/TCHSStokes/Tests.cmake
@@ -47,3 +47,18 @@ AddTest(
     pipe_ref_1000s.vtu pipe_pcs_0_ts_100_t_1000.000000.vtu xmV_ref mass_fraction_interpolated 1e-15 1e-12
     pipe_ref_1000s.vtu pipe_pcs_0_ts_100_t_1000.000000.vtu v_Darcy_ref darcy_velocity 1e-15 1e-15
 )
+
+AddTest(
+    NAME TCHSStokes_test_fluid_eos
+    PATH Parabolic/TCHSStokes/test-fluid-eos
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS pipe.prj
+    WRAPPER time
+    TESTER vtkdiff
+    REQUIREMENTS NOT OGS_USE_MPI
+    DIFF_DATA
+    pipe_ref.vtu pipe_pcs_0_ts_20_t_200.000000.vtu p_ref pressure_interpolated 1e-15 2e-7
+    pipe_ref.vtu pipe_pcs_0_ts_20_t_200.000000.vtu T_ref temperature_interpolated 1e-15 1e-13
+    pipe_ref.vtu pipe_pcs_0_ts_20_t_200.000000.vtu xmV_ref mass_fraction_interpolated 1e-15 1e-15
+    pipe_ref.vtu pipe_pcs_0_ts_20_t_200.000000.vtu v_Darcy_ref darcy_velocity 1e-10 1e-15
+)
--- Tests/Data/Parabolic/TCHSStokes/test-fluid-eos/prepare-meshes.py
+++ Tests/Data/Parabolic/TCHSStokes/test-fluid-eos/prepare-meshes.py
@@ -10,50 +10,42 @@ OGSPATH = "/home/lehmannc/prog/ogs6/github-chleh-PRs/build-release-eigen/bin"
 
 reader = ps.XMLUnstructuredGridReader(FileName=["pipe.vtu"])
 
-### Material Ids
+if False:
+    ### Material Ids
 
-def f_mat_ids(coords):
-    import numpy as np
-    return np.zeros(coords.shape[0]).astype("i")
+    def f_mat_ids(coords):
+        import numpy as np
+        return np.zeros(coords.shape[0]).astype("i")
 
-def f_v(coords):
-    import numpy as np
-    return np.zeros(coords.shape[0])
+    def f_v(coords):
+        import numpy as np
+        return np.zeros(coords.shape[0])
 
-def f_enum(coords):
-    import numpy as np
-    return np.arange(coords.shape[0], dtype=np.uint64)
+    def f_enum(coords):
+        import numpy as np
+        return np.arange(coords.shape[0], dtype=np.uint64)
 
-# mat_ids = pre.CellFunction("MaterialIDs", f_mat_ids, Input=reader)
-zero_v = pre.NodalFunction("zero", f_v, Input=reader)
-# profile_T.CopyArrays = 1
-enum_pts = pre.NodalFunction("OriginalSubsurfaceNodeIDs", f_enum, Input=zero_v)
-enum_pts.CopyArrays = 1
+    # mat_ids = pre.CellFunction("MaterialIDs", f_mat_ids, Input=reader)
+    zero_v = pre.NodalFunction("zero", f_v, Input=reader)
+    # profile_T.CopyArrays = 1
+    enum_pts = pre.NodalFunction("OriginalSubsurfaceNodeIDs", f_enum, Input=zero_v)
+    enum_pts.CopyArrays = 1
 
-# save data
-ps.SaveData("pipe_zero_v.vtu", proxy=enum_pts, DataMode='Binary',
-    EncodeAppendedData=1,
-    CompressorType='ZLib')
-
-
-sys.exit()
+    # save data
+    ps.SaveData("pipe_zero_v.vtu", proxy=enum_pts, DataMode='Binary',
+        EncodeAppendedData=1,
+        CompressorType='ZLib')
 
 
 # reference solution
 
-def f_profile_T_500(coords):
+def f_profile_T(coords):
     import numpy as np
-    return 3e2 + 3e1 * np.sin(np.pi*coords[:,0]) * np.sin(np.pi*coords[:,1]) \
-            * np.exp(-2.0 * 0.00114443464762167888 * 500)
-
-def f_profile_T_1000(coords):
-    import numpy as np
-    return 3e2 + 3e1 * np.sin(np.pi*coords[:,0]) * np.sin(np.pi*coords[:,1]) \
-            * np.exp(-2.0 * 0.00114443464762167888 * 1000)
+    return 4e2 * np.ones(coords.shape[0])
 
 def f_p_ref(coords):
     import numpy as np
-    return 1e5 * np.ones(coords.shape[0])
+    return (4./3.)* 1e5 * np.ones(coords.shape[0])
 
 def f_xmV_ref(coords):
     import numpy as np
@@ -63,7 +55,7 @@ def f_v_Darcy_ref(coords):
     import numpy as np
     return np.zeros((coords.shape[0], 2))
 
-T_ref = pre.NodalFunction("T_ref", f_profile_T_500, Input=reader)
+T_ref = pre.NodalFunction("T_ref", f_profile_T, Input=reader)
 p_ref = pre.NodalFunction("p_ref", f_p_ref, Input=T_ref)
 p_ref.CopyArrays = 1
 xmV_ref = pre.NodalFunction("xmV_ref", f_xmV_ref, Input=p_ref)
@@ -72,20 +64,6 @@ v_ref = pre.NodalFunction("v_Darcy_ref", f_v_Darcy_ref, Input=xmV_ref)
 v_ref.CopyArrays = 1
 
 # save data
-ps.SaveData("pipe_ref_500s.vtu", proxy=v_ref, DataMode='Binary',
-    EncodeAppendedData=1,
-    CompressorType='ZLib')
-
-
-T_ref = pre.NodalFunction("T_ref", f_profile_T_1000, Input=reader)
-p_ref = pre.NodalFunction("p_ref", f_p_ref, Input=T_ref)
-p_ref.CopyArrays = 1
-xmV_ref = pre.NodalFunction("xmV_ref", f_xmV_ref, Input=p_ref)
-xmV_ref.CopyArrays = 1
-v_ref = pre.NodalFunction("v_Darcy_ref", f_v_Darcy_ref, Input=xmV_ref)
-v_ref.CopyArrays = 1
-
-# save data
-ps.SaveData("pipe_ref_1000s.vtu", proxy=v_ref, DataMode='Binary',
+ps.SaveData("pipe_ref.vtu", proxy=v_ref, DataMode='Binary',
     EncodeAppendedData=1,
     CompressorType='ZLib')
