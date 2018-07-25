901f2d8e85d4b8373b30f4191933a7c52a5710c7
Christoph Lehmann <christoph.lehmann@ufz.de>
2018-07-25 09:55:24 +0200

    restricted node detection

--- Tests/Data/Mechanics/Linear/Python/hertz-pressure-bc.py
+++ Tests/Data/Mechanics/Linear/Python/hertz-pressure-bc.py
@@ -1,28 +1,52 @@
 import OpenGeoSys
 
-def get_y_top(t):
-    return 1.0 - 0.005 * t
-
 class BC(OpenGeoSys.BoundaryCondition):
-    def getDirichletBCValue(self, t, coords, node_id, primary_vars):
-        y = coords[1]
+    def __init__(self):
+        super(BC, self).__init__()
+        self._restricted_node_xs = set()
+        self._t_old = -1.0
 
-        # return (True, -0.1 * y)
+    def getDirichletBCValue(self, t, coords, node_id, primary_vars):
+        if self._t_old < t:
+            # first iteration in current timestep
+            print("restricted nodes in prev. ts", self._restricted_node_xs)
+            self._restricted_node_xs.clear()
+            self._t_old = t
+
+        x, y, z = coords
 
         y_deformed = y + primary_vars[1]
-        y_top = get_y_top(t)
+        y_top = BC.get_y_top(t)
         print("y_top", y_top)
-        # if y_deformed > y_top - 1e-10:
         if y_deformed >= y_top:
             print("XXXXX def!", y, y_top, y_top - y)
-            # return (True, y_top - y)
-            return (True, y_top - y)
-            # return (True, -0.01 * y)
+            if self.is_new_restricted_node(x):
+                res = (True, y_top - y)
+                self._restricted_node_xs.add(x)
+            elif self.is_rightmost_restricted_node(x):
+                res = (False, 0.0)
+            else:
+                res = (True, y_top - y)
+        else:
+            res =(False, 0.0)
 
-        return (False, 0.0)
+        self._t_old = t
+        return res
 
-    # def getFlux(self, t, coords, primary_vars):
-    #     print(">N", t, coords, primary_vars, type(primary_vars))
-    #     return (True, 0.0, [ 1.0, 1.0 ])
+    @staticmethod
+    def get_y_top(t):
+        return 1.0 - 0.005 * t
+
+    def is_first_iteration(self, x):
+        # TODO better detection
+        # return x in self._restricted_node_xs
+        return len(self._restricted_node_xs) == 0 or x > max(self._restricted_node_xs)
+
+    def is_new_restricted_node(self, x):
+        return len(self._restricted_node_xs) == 0 or x > max(self._restricted_node_xs)
+
+    def is_rightmost_restricted_node(self, x):
+        return len(self._restricted_node_xs) != 0 and x == max(self._restricted_node_xs)
+
 
 bc = BC()
