a9a46d3894c63935781067b7634e47b8d6d888f5
Christoph Lehmann <christoph.lehmann@ufz.de>
2018-07-25 11:49:03 +0200

    WIP Hertz contact

--- Tests/Data/Mechanics/Linear/Python/hertz-pressure-bc.py
+++ Tests/Data/Mechanics/Linear/Python/hertz-pressure-bc.py
@@ -1,5 +1,7 @@
 import OpenGeoSys
 
+NODE_RELEASE_FRACTION = 0.5
+
 class BC(OpenGeoSys.BoundaryCondition):
     def __init__(self):
         super(BC, self).__init__()
@@ -17,36 +19,34 @@ class BC(OpenGeoSys.BoundaryCondition):
 
         y_deformed = y + primary_vars[1]
         y_top = BC.get_y_top(t)
+
         print("y_top", y_top)
-        if y_deformed >= y_top:
+        if y_deformed > y_top:
+            res = (True, y_top - y)
+            self._restricted_node_xs.add(x)
+        elif y_deformed > y_top - 1e-8:
             print("XXXXX def!", y, y_top, y_top - y)
             if self.is_new_restricted_node(x):
                 res = (True, y_top - y)
                 self._restricted_node_xs.add(x)
-            elif self.is_rightmost_restricted_node(x):
+            elif self.is_relaxed_restricted_node(x):
                 res = (False, 0.0)
             else:
                 res = (True, y_top - y)
         else:
             res =(False, 0.0)
 
-        self._t_old = t
         return res
 
     @staticmethod
     def get_y_top(t):
         return 1.0 - 0.005 * t
 
-    def is_first_iteration(self, x):
-        # TODO better detection
-        # return x in self._restricted_node_xs
-        return len(self._restricted_node_xs) == 0 or x > max(self._restricted_node_xs)
-
     def is_new_restricted_node(self, x):
         return len(self._restricted_node_xs) == 0 or x > max(self._restricted_node_xs)
 
-    def is_rightmost_restricted_node(self, x):
-        return len(self._restricted_node_xs) != 0 and x == max(self._restricted_node_xs)
+    def is_relaxed_restricted_node(self, x):
+        return len(self._restricted_node_xs) != 0 and x > (1.0 - NODE_RELEASE_FRACTION) * max(self._restricted_node_xs)
 
 
 bc = BC()
